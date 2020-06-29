79f625a89df489355d82527b542926f9db73bc7c
devanshiu <devanshi.udeshi@gmail.com>
2020-06-28 20:17:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieTrailerActivity.java
@@ -0,0 +1,34 @@
+package com.example.flixster;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_trailer);
+
+        final String videoKey = getIntent().getStringExtra("key");
+
+        YouTubePlayerView playerView = findViewById(R.id.player);
+
+        playerView.initialize(getString(R.string.api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo(videoKey);
+                youTubePlayer.play();
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+    }
+}
